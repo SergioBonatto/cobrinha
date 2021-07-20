e5fec5311a8d8579d6580dfbc577efd4f6787c5c
SergioBonatto <bonatto@tutanota.com>
2021-07-20 17:46:50 -0300

    criei função cobrinha andar1

--- script.js
+++ script.js
@@ -12,11 +12,18 @@ snake[0] = {
 }
 
 
+let direction = "right";
+
+// Funções
+
+// cria BG
 function criarBG() {
 	context.fillStyle = "lightgreen";
 	context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+
+// cria cobrinha
 function criarCobra() {
 	for(i = 0; i < snake.length; i++){
 		context.fillStyle = "black";
@@ -24,5 +31,34 @@ function criarCobra() {
 	}
 }
 
-criarBG();
-criarCobra();
+
+function iniciarJogo() {
+	//starta funções
+	criarBG();
+	criarCobra();
+
+	// setando posição inicial da cobrinha
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
+	//seta como vai andar
+	if(direction == "right") snakeX += box;
+	if(direction == "left") snakeX -= box;
+	if(direction == "up") snakeY -= box;
+	if(direction == "down") snakeY += box;
+	
+	//retirar box final para movimento
+	snake.pop();
+
+	//adiciona box inicial para movimento (cabeça)
+	let newHead = {
+		x: snakeX, 
+		y: snakeY
+	}
+
+	snake.unshift(newHead);
+
+
+}
+
+let jogo = setInterval(iniciarJogo, 100)
